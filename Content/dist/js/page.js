00a0fb5f0970a4354fee4ba89067ce76ad88c45f
lastsoup <qingtang166@qq.com>
2018-07-03 21:36:54 +0800

    new

---
+++ Content/dist/js/page.js
@@ -0,0 +1,18 @@
+var host = "http://192.168.31.242:8084/wfapi";
+var WFUrl = "http://192.168.31.242:8084/wfui/WorkflowProcessView.aspx";
+var InstanceStateEnum = { 0: "INITIALIZED", 1: "RUNNING", 7: "COMPLETED", 9: "CANCELED" };
+$(document).ready(function () {
+    $("#mainHeadName,#mainHeadActive").text("创建任务");
+    $("#mainHeadContent").text("任务列表");
+    x$('#maincontent').xhr('inner', './page/createtask.html?t=' + new Date().getTime());
+    $(".treeview-menu a").not($("#wfdesinger")).click(function () {
+        var href=$(this).attr("action-href");
+        var text=$(this).text();
+        var title=$(this).attr("action-title");
+        $(".treeview-menu li").removeClass("active");
+        $(this).parent().addClass("active");
+        $("#mainHeadName,#mainHeadActive").text(text);
+        $("#mainHeadContent").text(title);
+        x$('#maincontent').xhr('inner', './page/'+href+'.html?t=' + new Date().getTime());
+    });
+});
