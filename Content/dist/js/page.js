9613329c3b224db74d0dbdef151e288a8e65276e
lastsoup <qingtang166@qq.com>
2018-07-06 17:30:15 +0800

    add

--- Content/dist/js/page.js
+++ Content/dist/js/page.js
@@ -1,5 +1,5 @@
 var host = "https://58.213.48.24:3001";
-var WFUrl = "https://58.213.48.24:3002/WorkflowProcessView.aspx";
+var WFUrl = "http://58.213.48.24:3002/WorkflowProcessView.aspx";
 var InstanceStateEnum = { 0: "INITIALIZED", 1: "RUNNING", 7: "COMPLETED", 9: "CANCELED" };
 $(document).ready(function () {
     $("#mainHeadName,#mainHeadActive").text("创建任务");
@@ -14,19 +14,5 @@ $(document).ready(function () {
         $("#mainHeadName,#mainHeadActive").text(text);
         $("#mainHeadContent").text(title);
         x$('#maincontent').xhr('inner', './page/'+href+'.html?t=' + new Date().getTime());
-    });
-
-    $.get("/Designer/LoanProcess.xml",function(data){
-        var father=$(data)[0].firstElementChild;
-        var dd=father.getElementsByTagName("fpdl:StartNode");
-        console.log($(dd).attr("Id"));
-        console.log(data);
-    });
-
-     $.get("/Designer/Goods_Deliver_Process.xml",function(data){
-        var father=$(data)[0].firstElementChild;
-        var dd=father.getElementsByTagName("fpdl:StartNode");
-        console.log($(dd).attr("Id"));
-        console.log(data);
     });
 });
